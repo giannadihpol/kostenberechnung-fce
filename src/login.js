5158f6540812b725faa6e7e0f7d8717267bf5da2
giannadihpol <gdihpol@gmail.com>
2021-11-22 21:39:17 +0100

    initial commit

---
+++ src/login.js
@@ -0,0 +1,14 @@
+const button = document.querySelector("#button");
+
+
+button.addEventListener("click",async (evt) => {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if(username==="admin"&&password==="Projektarbeit-2"){
+        window.location.href = 'index2.html';
+    }
+    else{
+        alert("Login fehlerhaft");
+        
+    }
+})
